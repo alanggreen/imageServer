b9c47e8a7fcf1a7190824885cf05f23d0ad06e84
Alan Green <alanggreen@gmail.com>
2017-08-23 16:56:29 +0300

    commit index.js so user doesn't have to transpile

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import * as http from "http"
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const http = require("http");
 var staticAlias = require("node-static-alias");
 var log4js = require('log4js');
-
 var logger = log4js.getLogger('node-static-alias');
 const port = 1395;
-
 var fileServer = new staticAlias.Server('./images', {
     alias: [
         {
@@ -22,10 +22,10 @@ var fileServer = new staticAlias.Server('./images', {
     ],
     logger: console
 });
-
 http.createServer(function (request, response) {
     request.addListener('end', function () {
         fileServer.serve(request, response);
     }).resume();
 }).listen(1395);
 console.log('server running on port', port);
+//# sourceMappingURL=index.js.map
